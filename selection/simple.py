178508d847b1de2de3973263d4e8cf397a9fccb3
Luis Vega <vegaluisjose@gmail.com>
2020-06-27 18:45:22 -0700

    rename

--- selection/simple.py
+++ selection/simple.py
@@ -1,7 +1,6 @@
-class Info(object):
-    def __init__(self, cost, pattern, code):
+class Tile(object):
+    def __init__(self, cost, code):
         self.cost = cost
-        self.pattern = pattern
         self.code = code
 
 class Op(object):
@@ -9,7 +8,7 @@ class Op(object):
         self.opcode = opcode
         self.operands = operands
         self.dtype = dtype
-        self.info = None
+        self.tile = None
 
     def __getitem__(self, index):
         return self.operands[index]
@@ -24,11 +23,11 @@ class Op(object):
         return prefix
 
 class Pattern(object):
-    def __init__(self, opcode, operands, dtype, target="io", cost=0):
+    def __init__(self, opcode, operands, dtype, code="input", cost=0):
         self.opcode = opcode
         self.operands = operands
         self.dtype = dtype
-        self.target = target
+        self.code = code
         self.cost = cost
 
     def __getitem__(self, index):
@@ -50,23 +49,27 @@ class Pattern(object):
         return self.cost + sum(p.__estimate(n) for p, n in zip(self, node))
 
     def __estimate(self, node):
-        return node.info.cost if node.info is not None else float("inf")
+        return node.tile.cost if node.tile is not None else float("inf")
 
-def selection(code, patterns):
+def instruction_selection(code, patterns):
     for node in code.postorder([]):
         best = float('inf')
         for pat in patterns:
             if pat.match(node):
                 cost = pat.estimate(node)
                 if cost < best:
-                    print("found new best, opcode:{} target:{}".format(pat.opcode, pat.target))
                     best = cost
-                    node.info = Info(cost, pat, code)
+                    node.tile = Tile(cost, pat.code)
+
+def codegen(code):
+    for node in code.postorder([]):
+        print(node.tile.cost, node.tile.code)
 
 if __name__ == "__main__":
     code = Op("add", [Op("input", [], 8), Op("input", [], 8)], 8)
     patterns = []
-    patterns.append(Pattern("input", [], 8, target="io"))
-    patterns.append(Pattern("add", [Pattern("input", [], 8), Pattern("input", [], 8)], 8, target="dsp", cost=1))
-    patterns.append(Pattern("add", [Pattern("input", [], 8), Pattern("input", [], 8)], 8, target="lut", cost=2))
-    selection(code, patterns)
+    patterns.append(Pattern("input", [], 8, code="input"))
+    patterns.append(Pattern("add", [Pattern("input", [], 8), Pattern("input", [], 8)], 8, code="dsp_add", cost=1))
+    patterns.append(Pattern("add", [Pattern("input", [], 8), Pattern("input", [], 8)], 8, code="lut_add", cost=2))
+    instruction_selection(code, patterns)
+    codegen(code)
