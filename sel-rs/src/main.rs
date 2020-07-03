40c328f5c1a8613afdc3ae5f4a6105bd6bb17601
Luis Vega <vegaluisjose@gmail.com>
2020-07-03 14:59:53 -0700

    add loc and refactor

--- sel-rs/src/main.rs
+++ sel-rs/src/main.rs
@@ -6,7 +6,7 @@ pub struct Node {
     name: String,
     lhs: Option<Rc<Node>>,
     rhs: Option<Rc<Node>>,
-    value: Option<String>,
+    value: Option<Instr>,
     cost: u128,
 }
 
@@ -45,27 +45,66 @@ impl Node {
 }
 
 #[derive(Clone, Debug)]
+pub enum Loc {
+    Gen,
+    Dsp,
+    Lut,
+}
+
+impl fmt::Display for Loc {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Loc::Gen => write!(f, "??"),
+            Loc::Dsp => write!(f, "dsp"),
+            Loc::Lut => write!(f, "lut"),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct Instr {
     opcode: String,
     dst: String,
     lhs: String,
     rhs: String,
+    loc: Loc,
 }
 
 impl Instr {
-    pub fn new_with_attrs(opcode: &str, dst: &str, lhs: &str, rhs: &str) -> Instr {
+    pub fn new_gen_instr(opcode: &str, dst: &str, lhs: &str, rhs: &str) -> Instr {
         Instr {
             opcode: opcode.to_string(),
             dst: dst.to_string(),
             lhs: lhs.to_string(),
             rhs: rhs.to_string(),
+            loc: Loc::Gen,
+        }
+    }
+
+    pub fn new_dsp_instr(opcode: &str, dst: &str, lhs: &str, rhs: &str) -> Instr {
+        Instr {
+            opcode: opcode.to_string(),
+            dst: dst.to_string(),
+            lhs: lhs.to_string(),
+            rhs: rhs.to_string(),
+            loc: Loc::Dsp,
+        }
+    }
+
+    pub fn new_lut_instr(opcode: &str, dst: &str, lhs: &str, rhs: &str) -> Instr {
+        Instr {
+            opcode: opcode.to_string(),
+            dst: dst.to_string(),
+            lhs: lhs.to_string(),
+            rhs: rhs.to_string(),
+            loc: Loc::Lut,
         }
     }
 }
 
 impl fmt::Display for Instr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {} {} {}", self.opcode, self.dst, self.lhs, self.rhs)
+        write!(f, "{} {} {} {} @{}", self.opcode, self.dst, self.lhs, self.rhs, self.loc)
     }
 }
 
@@ -81,8 +120,16 @@ impl Prog {
         }
     }
 
-    pub fn create_instr(&mut self, opcode: &str, dst: &str, lhs: &str, rhs: &str) {
-        self.body.push(Instr::new_with_attrs(opcode, dst, lhs, rhs));
+    pub fn create_gen_instr(&mut self, opcode: &str, dst: &str, lhs: &str, rhs: &str) {
+        self.body.push(Instr::new_gen_instr(opcode, dst, lhs, rhs));
+    }
+
+    pub fn create_dsp_instr(&mut self, opcode: &str, dst: &str, lhs: &str, rhs: &str) {
+        self.body.push(Instr::new_dsp_instr(opcode, dst, lhs, rhs));
+    }
+
+    pub fn create_lut_instr(&mut self, opcode: &str, dst: &str, lhs: &str, rhs: &str) {
+        self.body.push(Instr::new_lut_instr(opcode, dst, lhs, rhs));
     }
 }
 
@@ -90,7 +137,7 @@ impl fmt::Display for Prog {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut body = String::new();
         for instr in self.body.iter() {
-            body.push_str(&format!("\n{}", instr.to_string()));
+            body.push_str(&format!("\n{};", instr.to_string()));
         }
         write!(f, "{}", body)
     }
@@ -98,8 +145,8 @@ impl fmt::Display for Prog {
 
 fn create_program() -> Prog {
     let mut prog = Prog::new();
-    prog.create_instr("mul", "t0", "a", "b");
-    prog.create_instr("add", "t1", "t0", "c");
+    prog.create_gen_instr("mul", "t0", "a", "b");
+    prog.create_gen_instr("add", "t1", "t0", "c");
     prog
 }
 
